e7bd6cede414e312601e5c564da64ce7960fd3d2
abdulloyev <abdulloyev.uz@gmail.com>
2025-03-24 21:36:42 +0500

    #creat - card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
 
 import React from "react";
+import { getData } from "./constants/db";
+import Card from "./components/card/card";
 
 const App = () => {
-  return <div>App</div>;
+  // Courses contained dataBase
+  const courses = getData();
+
+  return (
+    <>
+      <h1 className="heading">Sammi kurslari</h1>
+
+      {/* Cards */}
+      <div className="cards_container">
+        {courses.map(course => (
+          <Card key={course.id} course={course} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
